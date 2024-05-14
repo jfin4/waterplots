303a059dd1d22d2659c88b347e524ea6721f8a9d
John Inman <john@johninman.dev>
2024-05-13 18:06:42 -0700

    changed map markers to ballon text with station labels

--- frontend/scripts/map.js
+++ frontend/scripts/map.js
@@ -2,37 +2,43 @@ var map = L.map('mapid').setView([37.17, -119.45], 7);
 var popup = document.getElementById('popup');
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-                attribution: '© OpenStreetMap © CARTO',
-                maxZoom: 20
-            }).addTo(map);
+        attribution: '© OpenStreetMap © CARTO',
+        maxZoom: 20
+}).addTo(map);
 
-var markers = L.markerClusterGroup(); 
+var markers = L.markerClusterGroup();
 
 function openPopup(station) {
-    var popup = document.getElementById('popup');
-    // var popupContent = document.getElementById('popup-content');
-    var popupTitle = document.getElementById('popup-title');
-
-    // popupContent.textContent = station.code;
-    popupTitle.textContent = station.code;
-
-    popup.style.display = 'block';
+        var popupTitle = document.getElementById('popup-title');
+        popupTitle.textContent = station.code;
+        popup.style.display = 'block';
 }
 
 map.on('click', function () {
-    document.getElementById('popup').style.display = 'none';
+        document.getElementById('popup').style.display = 'none';
 });
+
+function createCustomIcon(station) {
+        return L.divIcon({
+                    className: 'custom-icon',
+                    html: '<div class="icon-content">' + station.code + '</div><div class="pointer"></div>',
+                    iconSize: [2, 30],
+                    iconAnchor: [1, 30]
+                });
+}
 
 fetch('/stations')
     .then(response => response.json())
     .then(data => {
-        data.forEach(station => {
-            var marker = L.marker([station.latitude, station.longitude])
-                .on('click', function() {
-                    openPopup(station);
-                });
-            markers.addLayer(marker);
-        });
-        map.addLayer(markers);
-    })
+                data.forEach(station => {
+                                var customIcon = createCustomIcon(station);
+                                var marker = L.marker([station.latitude, station.longitude], { icon: customIcon })
+                                    .on('click', function() {
+                                                            openPopup(station);
+                                                        });
+                                markers.addLayer(marker);
+                            });
+                map.addLayer(markers);
+            })
     .catch(e => console.error(e));
+
